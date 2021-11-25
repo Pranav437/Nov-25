95af4c9ac2e71ac234e50e4829792ce286ce5b78
Pranav437 <pranavece58@gmail.com>
2021-11-25 00:30:23 -0500

    First commit

---
+++ src/Learning/Arry.java
@@ -0,0 +1,23 @@
+package Learning;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Iterator;
+
+public class Arry {
+
+    public static void main(String[] args) {
+	ArrayList <Integer> value = new ArrayList<>();
+value.add(2);
+value.add(5);
+value.add(8);
+value.add(7);
+value.add(58);
+        System.out.println(value);
+        System.out.println(value.contains(4));
+        value.clear();
+        System.out.println(value);
+
+
+            }
+}
---
+++ src/Learning/Linky.java
@@ -0,0 +1,15 @@
+package Learning;
+
+import java.util.LinkedList;
+
+public class Linky {
+    public static void main(String[] args) {
+
+        LinkedList value = new LinkedList();
+        value.add("Hello");
+        value.add("World");
+        value.add("Music");
+        value.add("Guitar");
+        System.out.println(value);
+    }
+}
